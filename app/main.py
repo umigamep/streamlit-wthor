7adca582d9ca5fb3a30b7ffb2d60adea33daf2b3
Yasushi Nagano <27348322+umigamep@users.noreply.github.com>
2023-12-20 23:09:25 +0900

    update main page

--- app/main.py
+++ app/main.py
@@ -7,11 +7,25 @@ st.set_page_config(
 )
 
 # Title
-st.title("wthorデータベース可視化ツール")
+st.title("(開発中)wthorデータベース可視化ツール")
 
 st.write(
     """
     wthorデータベースの棋譜を集計・分析した結果を可視化するWebアプリです。
+
+    左のタブから分析観点を選べます。
+    
+    - compare_year: 年ごとのトッププレイヤーの石損の指標を比較します
+    - compare_palyer: プレイヤーごとの石損指標を比較します
+    - search_games: (開発中)着手の統計情報や棋譜からゲームを検索する機能です
+
     ### 参考リンク
+
+    - [wthorデータベース](https://www.ffothello.org/informatique/la-base-wthor/)
+        - フランスオセロ連盟が公開している棋譜データベース
+    - [edax](https://github.com/abulmo/edax-reversi)
+        - 解析にLv18+手持ちのbookを使用しました
+    
+
     """
 )
